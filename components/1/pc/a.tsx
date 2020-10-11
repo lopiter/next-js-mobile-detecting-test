749686dab1f2788d28b4993c793285b4f35351a3
lopiter <lopiter@revhat.com>
2020-10-11 09:17:57 +0900

    initial

---
+++ components/1/pc/a.tsx
@@ -0,0 +1,8 @@
+export default function info() {
+    return (
+        <>
+        <div className="materialize-red123">i am pc</div>
+        <img src="http://image.dongascience.com/Photo/2018/01/15159739972169[1].jpg"></img>
+        </>
+    )
+}
